096e980ac62fb7f6b22da13a6d4fb14b87478f3d
Matt Cabarrubias <monapwc2020@gmail.com>
2025-11-04 22:25:23 +0800

    feat(backend): Added DTOs: AuthResponse, LoginRequest, MessageResponse and RegisterRequest

---
+++ backend/src/main/java/com/dcisman/dto/AuthResponse.java
@@ -0,0 +1,29 @@
+package com.dcisman.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class AuthResponse {
+
+    private String token;
+    @Builder.Default
+    private String type = "Bearer";
+    private Long id;
+    private String username;
+    private String email;
+    private String role;
+
+    public AuthResponse(String token, Long id, String username, String email, String role) {
+        this.token = token;
+        this.id = id;
+        this.username = username;
+        this.email = email;
+        this.role = role;
+    }
+}
